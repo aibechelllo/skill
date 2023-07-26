1311597a3aaf42d5847f0cf8940c0b6ada4a97f6
aibechelllo <iplaydota4sk@gmail.com>
2023-07-26 15:53:54 +0600

    asd

--- started/layout_lesson/js_intro_lesson/index.js
+++ started/layout_lesson/js_intro_lesson/index.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+// window.addEventListener('DOMContentLoaded', function(){
+//     console.log(document.querySelector('.block'))
+// })
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     console.log(document.querySelectorAll('.text'))
+// })
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     document.querySelector('.text').style.color = 'blue'; 
+// })
+
+// window.addEventListener('DOMContentLoaded', function(){
+//     document.querySelector('#text').classList.add('alert')
+//     document.querySelector('#text').classList.remove('alert')
+// })
+
+window.addEventListener('DOMContentLoaded', function(){
+    this.document.querySelectorAll('.text').forEach(function(element){
+        element.classList.add('alert')
+    })
+})
