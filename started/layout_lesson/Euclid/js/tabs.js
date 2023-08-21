5ce985cea114dc263e633be19b269c0bff8bbb6d
aibechelllo <iplaydota4sk@gmail.com>
2023-08-21 13:44:47 +0600

    asd

--- started/layout_lesson/Euclid/js/tabs.js
+++ started/layout_lesson/Euclid/js/tabs.js
@@ -2,7 +2,11 @@ document.addEventListener('DOMContentLoaded', function(){
     document.querySelectorAll('.section-how__steps-btn').forEach(function(stepsBtn){
         stepsBtn.addEventListener('click', function(event) {
             const path = event.currentTarget.dataset.path
-            console.log(path)
+
+            document.querySelectorAll('.section-how__steps-info-item').forEach(function(stepsInfoItem){
+                stepsInfoItem.classList.remove('section-how__steps-info-item-active')
+            })
+            document.querySelector(`[data-target="${path}"]`).classList.add('section-how__steps-info-item-active')
         })
     })
 });
