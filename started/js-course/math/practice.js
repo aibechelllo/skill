fe0e73ca74f18c2fae91b3831d2f684ad4934edd
aibechelllo <iplaydota4sk@gmail.com>
2023-09-15 15:00:37 +0600

    asd

--- started/js-course/math/practice.js
+++ started/js-course/math/practice.js
@@ -1,5 +1,18 @@
 /* 
 Вычисляем количество квартир в доме.
 Дано кол-во подъездов, этажей и квартир на одном этаже.
-Нужно вывести кол-во квартир в одном подъезде и во всём дома
+Нужно вывести кол-во квартир в одном подъезде и во всём
 */
+
+// кол-во подъездов
+let entrances = 4;
+//qty floors
+let floors = 9;
+//кол-во квартир на этаже 
+let flatsPerFloor = 4;
+
+let flatsPerEntrance = floors * flatsPerFloor;
+console.log("кол-во квартир в подъезде", flatsPerEntrance)
+
+let flats = entrances * flatsPerEntrance;
+console.log("кол-во квартир в доме", flats)
