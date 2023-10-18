b45472ed9f9a3f61110fcbd6f08d00a4bb911eaa
aibechelllo <iplaydota4sk@gmail.com>
2023-10-18 15:17:01 +0600

    asd

--- started/js-course/math/arrays.js
+++ started/js-course/math/arrays.js
@@ -1,1 +1,15 @@
-let fruits = ['apple', 'orange', 'banana', 'mango', 'avocado']
+let fruits = ['apple', 'orange', 'banana', 'mango', 'avocado'];
+
+let alphabet = [];
+
+//добавить данные в конце массива
+alphabet.push('G');
+alphabet.push('D');
+alphabet.push('E', 'J', 'Z');
+
+
+//добавить данные в начало
+alphabet.unshift('C');
+alphabet.unshift('A', 'B');
+
+console.log(alphabet);
